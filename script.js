65027c3e5196afb6b33bc4ce44b5b969a6d42379
Sergii <33702909+Gafigaf@users.noreply.github.com>
2020-02-23 18:18:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -47,25 +47,41 @@ function countt() {
 	hop.style.color = "#FFF";
 	var a1 = parseInt(document.getElementById("first").value);
 	var b2 = parseInt(document.getElementById("second").value);
-	document.getElementById("hop").innerHTML = "(" + a1 + "+" + b2 +")² = " + a1*a1 + "+"+2*a1*b2 +"+"+ b2*b2 ;
+	document.getElementById("hop").innerHTML = "(" + a1 + "+" + b2 +")² = " + a1*a1 + "+"+2*a1*b2 +"+"+ b2*b2 + "= " + parseInt(a1*a1 + 2 * a1*b2 + b2*b2);
 	if (isNaN(a1,b2)) {
-		document.getElementById("hop").innerHTML = "ВВЕДИТЕ ЧИСЛА";
+		swal("Ошибка", "ВВЕДИТЕ ЧИСЛА", "error");
+		document.getElementById("hop").innerHTML = "";
 		hop.style.color = "red";
 	}
 	var chbox;
 	var chbox2;
+	var bukva =(document.getElementById("buk").value);
+	var bukva2 = (document.getElementById("buk2").value);
+	var b3 = bukva +"²"+ bukva2;
+	var b4 = bukva + bukva2;
 	chbox = document.getElementById('check');
 	chbox2 = document.getElementById('check2');
 	if (chbox.checked) {
-		document.getElementById("hop").innerHTML = "(" + a1 +"a"+"+" + b2 +")² = " + a1*a1+ "a²" + "+"+2*a1*b2+"a"+"+"+ b2*b2 ;	
+		document.getElementById("hop").innerHTML = "(" + a1 +bukva+"+" + b2 +")² = " + a1*a1+ bukva+"²"+ "+"+2*a1*b2+bukva+"+"+ b2*b2;
+		if(isFinite(bukva)){
+			swal("Ошибка", "ВЫ ВВЕЛИ ЦИФРУ", "error");
+			document.getElementById("hop").innerHTML = "";
+			hop.style.color = "red";
+		}
 	}
 
 	if (chbox2.checked) {
 		document.getElementById("hop").innerHTML = "(" + a1 +"+" + b2+"a"+")² = " + a1*a1+ "+"+2*a1*b2+"a"+"+"+ b2*b2+"a²";
+		document.getElementById("hop").innerHTML = "(" + a1 +"+" + b2 +bukva2 +")² = " + a1*a1+"+"+2*a1*b2 + bukva2 +"+"+ b2*b2 + bukva2 + "²";
+		if(isFinite(bukva2)){
+			swal("Ошибка", "Вы ввели цифру / Вы ничего не ввели", "error");
+			document.getElementById("hop").innerHTML = "";
+			hop.style.color = "red";
+		}
 	}
 
 	if (chbox.checked && chbox2.checked) {
-		document.getElementById("hop").innerHTML = "(" + a1 +"a"+"+" + b2+"a"+")² = " + a1*a1+ "a²" + "+"+2*a1*b2+"a²"+"+"+ b2*b2+"a²";
+		document.getElementById("hop").innerHTML = "(" + a1 +b4+"+" + b2+b4+")² = " + a1*a1+b3+"²" + "+"+2*a1*b2+b3+"²"+"+"+ b2*b2+b3+"²" +"= "+ parseInt(a1*a1+2*a1*b2+b2*b2)+b3+"²";
 	}
 
 
@@ -73,7 +89,9 @@ function countt() {
 //Ochistka Kvadrat sumy
 
 function ochistt() {
+	document.getElementById("buk").value = '';
 	document.getElementById("first").value = '';
 	document.getElementById("second").value = '';
 	document.getElementById("hop").innerHTML = "";
+	document.getElementById("buk2").value = '';
 }
